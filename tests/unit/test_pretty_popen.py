b1103be1afa4f0bc365628aecbea1de903807942
tevos matevosyan <97086468+tevops@users.noreply.github.com>
2022-09-06 22:44:36 +0400

    Update test_pretty_popen.py

--- tests/unit/test_pretty_popen.py
+++ tests/unit/test_pretty_popen.py
@@ -2,7 +2,6 @@ import subprocess
 from pathlib import Path
 from typing import Union, Dict
 from pytest import mark
-import time
 
 from pretty_popen import PrettyPopen
 
@@ -61,5 +60,4 @@ def test_yaml():
 
 
 def test_json():
-    time.sleep(100)
     return func(cmd=args_json)
